6a30666ae99f5eada5724515079ee9fcb578b48a
Georgie Mnemonic <georges.hinot@gmail.com>
2024-10-06 19:11:09 +0200

    Update CreateMaps-1.py
    Create maps automatically with QGIS and PyQgis!
    Using APIs (OSM, Wikipedia, Wikidata) and geo-processing (intersection).
    
    Checked with QGIS 3.28.13
    Checked with QGIS 3.34.11
    
    Example of map produced:
    https://hg-map.fr/images/cours/python/2024-10-06-Grand-Pic-de-la-Meije.png
    
    Just change the path in "monCheminDeBase".
    Download the data examples below this article, and unzip them:
    https://hg-map.fr/tutos/73-qgis-et-python
    
    Then just run this code!
    
    Other PyQgis tips here:
    https://hg-map.fr/astuces/84-pyqgis
    
    Other standalone PyQgis tips here:
    https://hg-map.fr/astuces/85-pyqgis

--- CreateMaps-1.py
+++ CreateMaps-1.py
@@ -5,6 +5,9 @@ Using APIs (OSM, Wikipedia, Wikidata) and geo-processing (intersection).
 Checked with QGIS 3.28.13
 Checked with QGIS 3.34.11
 
+Example of map produced:
+https://hg-map.fr/images/cours/python/2024-10-06-Grand-Pic-de-la-Meije.png
+
 Just change the path in "monCheminDeBase".
 Download the data examples below this article, and unzip them:
 https://hg-map.fr/tutos/73-qgis-et-python
